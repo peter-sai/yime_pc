6cd859b9502d62b184c1b882b09b0c03c979d669
peter-sai <lanhu_l@outlook.com>
2021-12-20 16:21:59 +0800

    添加 electron

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 module.exports = {
+  // 这个值也可以被设置为相对路径 ('./')，这样所有的资源都会被链接为相对路径，这样打出来的包可以被部署在任意路径。
   publicPath: './',
+  // 输出文件目录
+  outputDir: 'build/vue-build',
   devServer: {
+    port: 8000,
     // https: true,
     // proxy: 'http://18.167.158.191:3000',
     proxy: 'http://localhost:3000',
