62d92c69a75c8863d4a060b5d5f5498d399e19c1
peter <peter@admin.com>
2022-03-02 11:27:21 +0800

    去掉超时时间

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -4,7 +4,7 @@ const request = axios.create({
   baseURL:
     process.env.NODE_ENV === 'development' ? '' : 'http://18.166.162.101:3000',
   // baseURL: process.env.NODE_ENV === 'development' ? '' : 'https://web.yime.app',
-  timeout: 20000,
+  // timeout: 20000,
 });
 
 request.interceptors.request.use(
