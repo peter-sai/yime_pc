a0cf83626a8d40a4afe53a2fc25b1a1dc8798d62
peter <lanhu_l@outlook.com>
2022-03-04 08:04:14 +0800

    修改打包过程

--- vue.config.js
+++ vue.config.js
@@ -47,4 +47,30 @@ module.exports = {
       return args;
     });
   },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        win: {
+          //win相关配置
+          target: [
+            {
+              target: 'nsis', //利用nsis制作安装程序
+              arch: [
+                'x64', //64位
+                'ia32', //32位
+              ],
+            },
+          ],
+        },
+        nsis: {
+          oneClick: false, // 是否一键安装
+          allowElevation: true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+          allowToChangeInstallationDirectory: true, // 允许修改安装目录
+          createDesktopShortcut: true, // 创建桌面图标
+          createStartMenuShortcut: true, // 创建开始菜单图标
+          shortcutName: 'demo', // 图标名称
+        },
+      },
+    },
+  },
 };
